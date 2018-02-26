7420536de577757c792a2b441ce41e39b498f5ab
chudakov <chudakov@hotmail.com>
2018-02-26 20:18:14 +0300

    -

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroesMock } from './mock-heroes';
+import { MessageService } from './message.service';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
@@ -9,9 +10,10 @@ import { of } from 'rxjs/observable/of';
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
+    this.messageService.add('Fetched heroes');
     var tmp = new HeroesMock();
     var a = tmp.HEROES; 
     return of(a);
